94d89faaf1e90619388635c9dac6af470448abf8
David Smith <codeactual@gmail.com>
2013-05-30 16:08:20 -0700

    chore(grunt): Migrate to grunt-horde

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,11 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  var mochaShelljsOpt = {stdout: true, stderr: false};
-
-  grunt.initConfig({
-    jshint: {
-      src: {
-        files: {
-          src: ['index.js', 'lib/**/*.js']
-        }
-      },
-      grunt: {
-        files: {
-          src: ['Gruntfile.js']
-        }
-      },
-      tests: {
-        options: {
-          expr: true
-        },
-        files: {
-          src: ['test/**/*.js']
-        }
-      },
-      json: {
-        files: {
-          src: ['*.json']
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'dist/weir.js': 'dist/weir.js'
-        }
-      }
-    },
-    shell: {
-      options: {
-        failOnError: true,
-        stdout: true,
-        stderr: true
-      },
-      build: {
-        command: 'component install --dev && component build --standalone weir --name weir --out dist --dev'
-      },
-      dist: {
-        command: 'component build --standalone weir --name weir --out dist'
-      },
-      test_lib: {
-        options: mochaShelljsOpt,
-        command: 'mocha --colors --async-only --reporter spec --recursive test/lib'
-      },
-      dox_lib: {
-        command: 'apidox --input lib/weir/index.js --output docs/Weir.md'
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dox', ['shell:dox_lib']);
-  grunt.registerTask('build', ['default', 'shell:build']);
-  grunt.registerTask('dist', ['default', 'shell:dist', 'uglify:dist', 'dox']);
-  grunt.registerTask('test', ['build', 'shell:test_lib']);
+  require('grunt-horde')
+    .create(grunt)
+    .demand('projName', 'weir')
+    .demand('instanceName', 'weir')
+    .demand('klassName', 'Weir')
+    .loot('node-component-grunt')
+    .attack();
 };
